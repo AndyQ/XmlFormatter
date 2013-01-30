621664ec20566169ff3d34ba567f96aab97f6195
AndyQ <andy.qua@gmail.com>
2013-01-30 10:26:46 +0000

    Initial Commit

---
+++ XmlFormater/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XmlFormater
+//
+//  Created by Andy Qua on 30/01/2013.
+//  Copyright (c) 2013 Andy Qua. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
